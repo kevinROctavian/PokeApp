07c158c074d6fbac0b8ce660199aab0b1c2592ee
Erick Renata <erick.renata@dataminesoftware.com>
2023-07-08 11:20:03 +0800

    Initial Commit

---
+++ PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by Erick Renata on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
